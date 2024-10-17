f0251ef1166b9da434ced16a004f1d97ab78b0f3
Pablo Mesa <pablo.mesa@nter.es>
2024-10-17 14:22:41 +0200

    feat: add @Operation annotations to AlbumController methods for improved API documentation.

--- src/main/java/com/example/pablomesaspringbootfundamentals/modules/album/controller/AlbumController.java
+++ src/main/java/com/example/pablomesaspringbootfundamentals/modules/album/controller/AlbumController.java
@@ -4,6 +4,7 @@ import com.example.pablomesaspringbootfundamentals.exception.ResourceNotFoundExc
 import com.example.pablomesaspringbootfundamentals.modules.album.dto.AlbumInputDTO;
 import com.example.pablomesaspringbootfundamentals.modules.album.dto.AlbumOutputDTO;
 import com.example.pablomesaspringbootfundamentals.modules.album.service.AlbumService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -25,6 +26,7 @@ public class AlbumController {
  * @param albumInputDTO the album input data transfer object containing the details of the album to be created
  * @return a ResponseEntity containing the created album output data transfer object and the HTTP status code CREATED
  */
+@Operation(summary = "Creates a new album", description = "Creates a new album with the provided details.")
 @PostMapping
 public ResponseEntity<AlbumOutputDTO> createAlbum(@Valid @RequestBody AlbumInputDTO albumInputDTO) {
     return new ResponseEntity<>(albumService.createAlbum(albumInputDTO), HttpStatus.CREATED);
@@ -35,6 +37,7 @@ public ResponseEntity<AlbumOutputDTO> createAlbum(@Valid @RequestBody AlbumInput
  *
  * @return a ResponseEntity containing a list of all album output data transfer objects and the HTTP status code OK
  */
+@Operation(summary = "Retrieves all albums", description = "Retrieves a list of all albums.")
 @GetMapping
 public ResponseEntity<List<AlbumOutputDTO>> getAllAlbums() {
     return ResponseEntity.ok(albumService.getAllAlbums());
@@ -47,6 +50,7 @@ public ResponseEntity<List<AlbumOutputDTO>> getAllAlbums() {
  * @return a ResponseEntity containing the album output data transfer object and the HTTP status code OK
  * @throws ResourceNotFoundException if the album with the specified ID is not found
  */
+@Operation(summary = "Retrieves an album by its ID", description = "Retrieves an album by its ID.")
 @GetMapping("/{id}")
 public ResponseEntity<AlbumOutputDTO> getAlbumById(@PathVariable Long id) {
     return ResponseEntity.ok(albumService.getAlbumById(id)
@@ -60,6 +64,7 @@ public ResponseEntity<AlbumOutputDTO> getAlbumById(@PathVariable Long id) {
  * @param albumInputDTO the album input data transfer object containing the updated details of the album
  * @return a ResponseEntity containing the updated album output data transfer object and the HTTP status code OK
  */
+@Operation(summary = "Updates an existing album", description = "Updates an existing album with the provided details.")
 @PutMapping("/{id}")
 public ResponseEntity<AlbumOutputDTO> updateAlbum(@PathVariable Long id, @Valid @RequestBody AlbumInputDTO albumInputDTO) {
     return ResponseEntity.ok(albumService.updateAlbum(id, albumInputDTO));
@@ -71,6 +76,7 @@ public ResponseEntity<AlbumOutputDTO> updateAlbum(@PathVariable Long id, @Valid 
  * @param id the ID of the album to be deleted
  * @return a ResponseEntity with no content and the HTTP status code NO_CONTENT
  */
+@Operation(summary = "Deletes an album by its ID", description = "Deletes an album by its ID.")
 @DeleteMapping("/{id}")
 public ResponseEntity<Void> deleteAlbum(@PathVariable Long id) {
     albumService.deleteAlbum(id);
